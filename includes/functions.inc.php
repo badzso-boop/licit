f56156c8489ad8b0b902097f0df703a0068dfee0
Badzso <norbert.ujj@gmail.com>
2023-03-28 23:09:41 +0200

    asd

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -149,16 +149,17 @@ function getSpecificUser($conn, $id) {
 }
 
 function updateUser($conn, $id, $name, $uname, $email, $bornDate, $type, $profileImg, $about, $links, $badge, $coupon, $level, $hobby, $work, $sport, $music) {
-	$sql = "UPDATE users SET uname=?, name=?, email=?,bornDate=?,type=?,profileImg=?,about=?,links=?,badge=?,coupon=?,level=?,hobby=?,work=?,sport=?,music=? WHERE id=?;";
-
-	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	$sql = "UPDATE users SET uname=$uname WHERE id=?;";
+	//$sql = "UPDATE users SET uname=?, name=?, email=?,bornDate=?,type=?,profileImg=?,about=?,links=?,badge=?,coupon=?,level=?,hobby=?,work=?,sport=?,music=? WHERE id=?;";
+	
 	$stmt = mysqli_stmt_init($conn);
 	if (!mysqli_stmt_prepare($stmt, $sql)) {
 	 	header("location: ../index.php?error=stmtfailed");
 		exit();
 	}
 
-	mysqli_stmt_bind_param($stmt, "sssssssssssssssi", $uname, $name, $email, $bornDate, $type, $profileImg, $about, $links, $badge, $coupon, $level, $hobby, $work, $sport, $music, $id);
+	//mysqli_stmt_bind_param($stmt, "sssssssssssssssi", $uname, $name, $email, $bornDate, $type, $profileImg, $about, $links, $badge, $coupon, $level, $hobby, $work, $sport, $music, $id);
+	mysqli_stmt_bind_param($stmt, "si", $uname, $id);
 	mysqli_stmt_execute($stmt);
 	mysqli_stmt_close($stmt);
 	mysqli_close($conn);
